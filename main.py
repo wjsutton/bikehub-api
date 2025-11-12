ae531f118aeac1fea2dc39c20aa40d82854b0080
Will Sutton <34423757+wjsutton@users.noreply.github.com>
2025-11-12 12:19:20 +0000

    Enhance main.py with progress print statements
    Added print statements to indicate the progress of the script.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from extract_bikepoint import extract
 from load import load
+print('loading functions')
 
 url = 'https://api.tfl.gov.uk/BikePoint'
 
+print('running extract')
 extract(url)
+print('extract done')
+
+print('running load')
 load()
+print('load done')
+
+print('script finished')
